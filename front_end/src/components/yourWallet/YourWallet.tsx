3b46bed7782c60a8ca012c83a0ae2ce056695cd4
huska0707 <hussaink001haled@gmail.com>
2024-05-30 13:59:24 -0500

    StyledTableRow

--- front_end/src/components/yourWallet/YourWallet.tsx
+++ front_end/src/components/yourWallet/YourWallet.tsx
@@ -32,6 +32,16 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   // },
 }));
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
   const { account } = useEthers();
 
