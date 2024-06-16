e4b8f2f8442fe8a2294c9ba3a61b032f3f2f6329
huska0707 <hussaink001haled@gmail.com>
2024-06-15 19:26:42 -0500

    Implement deposit function in AaveLending

--- contracts/AaveLending.sol
+++ contracts/AaveLending.sol
@@ -10,8 +10,44 @@ contract AaveLending is ILendingProtocol, Ownable {
     ILendingProtocol public pool;
     address public stakingContract;
 
+    modifier onlyStakingContract() {
+        require(
+            msg.sender == stakingContract;
+            "AaveLending: Caller is not Staking Contract or owner"
+        );
+        _;
+    }
+
     constructor(address _pool) {
         stakingContract = msg.sender;
         pool = ILendingPool(_pool);
+    }
+
+    function setStakingContract(address _stakingContract) external onlyOwner {
+        require(
+            _stakingContract != address(0),
+            "AaveLending: address given is 0x0"
+        );
+
+        stakingContract = _stakingContract;
+        emit StakingContractChange(_stakingContract);
+    }
+
+    function deposit(
+        address _token,
+        uint256 _amount,
+        address
+    ) external override(ILendingProtocol) onlyStakingContract {
+        require(
+            IERC20(_token).transferFrom(msg.sender, address(this), _amount),
+            "AaveLending: transferFrom() failed"
+        );
+
+        require(
+            IERC20(_token).approve(address(pool), _amount),
+            "AaveLending: approve() failed"
+        )
+
+        pool.deposit(_token, _amount, address(this), 0);
     }
 }
